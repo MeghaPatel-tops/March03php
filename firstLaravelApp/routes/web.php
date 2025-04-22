32611c5be6584abeb7599af4f0b1882adc12d833
Megha <meghapatel1.tops@gmail.com>
2025-04-22 12:00:36 +0530

    Laravel 21-04-2025 Topic:Routing

--- firstLaravelApp/routes/web.php
+++ firstLaravelApp/routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use  App\Http\Controllers\CommanController;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "<h1>Welcome to laravel app</h1>";
 });
+
+// Route::get('/home',function(){
+//     return view('home',["name"=>]);
+// });
+
+Route::view('/home', 'home');
+Route::view('/home', 'home',["name"=>"megha"]);
+
+Route::get('/test',[CommanController::class,'testcontroller'] );
+
+Route::get('/product',function(Request $req){
+     print_r($req->all());
+});
+
+Route::get('user/{id}',function($id){
+    echo $id;
+});
+
+Route::redirect('/here','test');
